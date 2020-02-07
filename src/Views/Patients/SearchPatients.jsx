d7b549e2c1f2480044e64c9eba3561ffa569a673
NANAA <nathan.lebon@epitech.eu>
2020-02-07 18:44:03 -0500

    SearchPatients.jsx fix

--- src/Views/Patients/SearchPatients.jsx
+++ src/Views/Patients/SearchPatients.jsx
@@ -1,40 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import './SearchPatients.css'
-import ObjectList from 'react-object-list'
 import searchlogo from '../../assets/search.svg'
 import PatientsSearchResults from "../../components/Search/PatientsSearchResults";
 
-class SearchPatients extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            searchTag: ''
-        }
-    }
+function SearchPatients() {
+    const [searchTag, setSearchTag] = useState('');
 
-
-    async handleChange(event) {
-        console.log(event.target.value)
-
-        await this.setState({searchTag: event.target.value})
-
-    }
-
-    render() {
-        return (
-          <div id="search-patients-container">
-            <div id="input-container">
-              <input type="text" placeholder="Rechercher un patient..." value={this.state.value} onChange={e => this.handleChange(e)} />
-              <img src={searchlogo} />
-            </div>
-
-            <div id="results-container">
-              <PatientsSearchResults searchTag={this.state.searchTag} />
-            </div>
-
-          </div>
-        )
-    }
+    return (
+      <div id="search-patients-container">
+        <div id="input-container">
+          <input type="text" placeholder="Rechercher un patient..." onChange={e => setSearchTag(e.target.value)} />
+          <img alt="" src={searchlogo} />
+        </div>
+        <div id="results-container">
+          <PatientsSearchResults searchTag={searchTag} />
+        </div>
+      </div>
+    )
 }
 
 export default SearchPatients;
