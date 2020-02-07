3466b10feb40495af0a955db9e5507008f25f318
NANAA <nathan.lebon@epitech.eu>
2020-02-07 14:08:06 -0500

    Renamed files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route} from 'react-router-dom';
 import Menu from "./components/Navigation/Menu";
-import SearchPatients from "./Views/Patients/SearchPatients";
 
 function App() {
   return (
