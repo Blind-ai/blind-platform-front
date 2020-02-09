03af503e7301bd303f69cfb5cc6a5c389bf9151c
NANAA <nathan.lebon@epitech.eu>
2020-02-09 13:50:14 -0500

    PersonalInfos

--- src/components/Patient/PersonalInfos.jsx
+++ src/components/Patient/PersonalInfos.jsx
@@ -1,45 +1,32 @@
 import React from "react";
 import './PersonalInfos.css'
 
-class PersonalInfos extends React.PureComponent{
-    constructor(props) {
-        super(props);
+const PersonalInfos = ({infos}) => {
 
-        this.state = {
-            infos : props.infos
-        }
-    }
-
-
-    // TODO a remplacer avec des grids
-    render() {
-        return (
-          <div id="personnalInfos-container">
-            <div id="personal-infos-row">
-              <div id="info-container">
-                <div className="info">Prénoms : </div>
-                <div className="info-data"> 
-                  {' '}
-                  {this.state.infos.firstname}
-                </div>
-              </div>
-              <div id="info-container">
-                <div className="info">Nom de Famille : </div>
-                <div className="info-data"> 
-                  {' '}
-                  {this.state.infos.lastname}
-                </div>
-              </div>
-              <div id="info-container">
-                <div className="info">Nom Marital : </div>
-                <div className="info-data">{this.state.infos.marital}</div>
-              </div>
-            </div>
-
+  // TODO a remplacer avec des grids
+  return (
+    <div id="personnalInfos-container">
+      <div id="personal-infos-row">
+        <div id="info-container">
+          <div className="info">Prénoms :</div>
+          <div className="info-data">
+            {infos.firstname}
           </div>
+        </div>
+        <div id="info-container">
+          <div className="info">Nom de Famille :</div>
+          <div className="info-data">
+            {infos.lastname}
+          </div>
+        </div>
+        <div id="info-container">
+          <div className="info">Nom Marital :</div>
+          <div className="info-data">{infos.marital}</div>
+        </div>
+      </div>
+    </div>
 
-        );
-    }
+  );
 }
 
 export default PersonalInfos
