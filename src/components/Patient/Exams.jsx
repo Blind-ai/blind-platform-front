b2205bfb15ceff49722e491a0fbe0b1eb6fa4cc6
NANAA <nathan.lebon@epitech.eu>
2020-02-09 14:00:57 -0500

    exams

--- src/components/Patient/Exams.jsx
+++ src/components/Patient/Exams.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import ModalImage from "react-modal-image";
 import './Exams.css'
 
 const Exams = ({id, exams}) => {
@@ -14,13 +15,16 @@ const Exams = ({id, exams}) => {
         }
         return examsList.map((exam, index) => {
             const {type, date, doctor, attachment} = exam;
+            const imageUrl = `http://188.166.53.41:5000/${  id  }/${  attachment}`;
 
             return (
               <div key={index} id="exam-row">
                 <div id="exam-info">{type}</div>
                 <div>{date}</div>
                 <div>{doctor}</div>
-                <img src={`http://188.166.53.41:5000/${  id  }/${  attachment}`} />
+                  <div style={{maxWidth: "300px"}}>
+                      <ModalImage small={imageUrl} large={imageUrl} alt={`Exam ${  date}`} />
+                  </div>
               </div>
             )
         })
